aec84f60c60ab7c70ab17dfec13c78d7851f4145
Vokturz <vnavarroaranguiz@gmail.com>
2025-06-29 16:15:20 +0100

    feat: Add KaTeX support for rendering math in Markdown

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,22 +1,52 @@
 import { Marked, Renderer } from 'marked'
 import hljs from 'highlight.js'
+import markedKatex from 'marked-katex-extension'
+
+const ensureMathBlockNewlines = {
+  async: false,
+  hooks: {
+    preprocess(markdown: string) {
+      return markdown.replace(/([^\n])\n\s*(\$\$)/g, '$1\n\n$2')
+    }
+  }
+}
 
 const renderer = new Renderer()
 
-renderer.code = function(token) {
-  const language = token.lang ? hljs.getLanguage(token.lang) ? token.lang : 'plaintext' : 'plaintext'
+renderer.code = function (token) {
+  const language = token.lang
+    ? hljs.getLanguage(token.lang)
+      ? token.lang
+      : 'plaintext'
+    : 'plaintext'
   return `<code-block data-code="${encodeURIComponent(token.text)}" data-lang="${language}"></code-block>`
 }
 
-renderer.link = function(token) {
+renderer.link = function (token) {
   const href = token.href
   const title = token.title ? ` title="${token.title}"` : ''
   const text = token.text
-  
-  const isExternal = href.startsWith('http://') || href.startsWith('https://') || href.startsWith('//')
+
+  const isExternal =
+    href.startsWith('http://') || href.startsWith('https://') || href.startsWith('//')
   const security = isExternal ? ' target="_blank" rel="noopener noreferrer"' : ''
-  
+
   return `<a class="underline hover:text-purple-500" href="${href}"${title}${security}>${text}</a>`
 }
 
-export const marked = new Marked({ renderer })
+const marked = new Marked()
+
+marked.use(
+  ensureMathBlockNewlines,
+  markedKatex({
+    // throwOnError: false // Set to true to see KaTeX errors
+  }),
+  {
+    renderer,
+    gfm: true
+  }
+)
+
+marked.use({ renderer })
+
+export { marked }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,11 @@
   import Footer from '$lib/components/layout/Footer.svelte'
 </script>
 
+<svelte:head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css">
+</svelte:head>
+
+
 <div class="text-foreground flex min-h-screen flex-col font-sans antialiased">
   <Header />
   <main class="container mx-auto max-w-5xl flex-grow px-4 py-8">
