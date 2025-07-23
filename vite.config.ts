cc9cdc3548a2ecc5a94607100502a014f5e0b4e4
Vokturz <vnavarroaranguiz@gmail.com>
2025-07-23 20:41:59 +0100

    Add vite-plugin-vercel for Vercel integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import tailwindcss from '@tailwindcss/vite'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
+import vercel from 'vite-plugin-vercel'
 // import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    sveltekit()
+    sveltekit(),
+    vercel()
     //mkcert()
   ],
   server: {
+    port: process.env.PORT as unknown as number,
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
       'Cross-Origin-Opener-Policy': 'same-origin'
