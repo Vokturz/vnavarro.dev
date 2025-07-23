91c498127e7d80499960bef0376b15e4885e8123
Vokturz <vnavarroaranguiz@gmail.com>
2025-07-23 21:39:13 +0100

    Set cross-origin security headers in handle hook
    Add Cross-Origin-Opener-Policy and Cross-Origin-Embedder-Policy headers
    to responses

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,8 @@
-// import type { Handle } from '@sveltejs/kit'
+import type { Handle } from '@sveltejs/kit'
 
-// export const handle: Handle = async ({ event, resolve }) => {
-//   const response = await resolve(event)
-
-//   response.headers.set('Cross-Origin-Embedder-Policy', 'require-corp')
-//   response.headers.set('Cross-Origin-Opener-Policy', 'same-origin')
-
-//   return response
-// }
+export const handle: Handle = async ({ event, resolve }) => {
+  const response = await resolve(event)
+  response.headers.set('Cross-Origin-Opener-Policy', 'same-origin')
+  response.headers.set('Cross-Origin-Embedder-Policy', 'require-corp')
+  return response
+}
