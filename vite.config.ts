3e7d94554139dbfc3f302d2cb72ca2617d698c74
Vokturz <vnavarroaranguiz@gmail.com>
2025-10-12 12:19:32 +0100

    add netlify plugin to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import tailwindcss from '@tailwindcss/vite'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
+import netlify from '@netlify/vite-plugin'
 // import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    sveltekit()
+    sveltekit(),
+    netlify()
     //mkcert()
   ],
   server: {
